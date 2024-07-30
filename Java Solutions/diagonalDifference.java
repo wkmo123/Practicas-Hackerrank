7f4b2fc30d706dd3e147a0b85b2b66516b88fbe3
Steven <juansf_456@hotmail.com>
2024-07-29 22:45:45 -0500

    diagonalDifference

---
+++ Java Solutions/diagonalDifference.java
@@ -0,0 +1,9 @@
+// Diagonal Difference
+//Ejercicio: https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+
+public static int diagonalDifference(List<Integer> arr) {
+    int diaDerecha = 0;
+    int diaIzquiera = 0;
+    
+}
